67d7f1f338e42aa7fe4567def31858c5409dbd37
John Matthews <jwmatthews@gmail.com>
2013-01-15 20:57:21 -0500

    Add a custom Salat context to fix error of: GRATER GLITCH when Salat doesn't have access to the Play classloader
    Caused by: com.novus.salat.util.GraterGlitch:
    
    GRATER GLITCH - unable to find or instantiate a grater using
    supplied path name
    
    REASON: Very strange!  Path='models.Registration' from
    pickled ScalaSig causes ClassNotFoundException
    
    Context: 'global'
    Path from pickled Scala sig: 'models.Registration'

--- app/models/Registration.scala
+++ app/models/Registration.scala
@@ -7,6 +7,28 @@ import com.novus.salat.global._
 import com.mongodb.casbah.Imports._
 import com.novus.salat.annotations._
 
+import com.novus.salat.{TypeHintFrequency, StringTypeHintStrategy, Context}
+import play.api.Play
+import play.api.Play.current
+
+/*
+  Adding a custom Salat context to work with Play's Classloader
+  Using example from:
+  https://github.com/leon/play-salat/blob/master/sample/app/models/mongoContext.scala
+*/
+package object mongoContext {
+  implicit val context = {
+    val context = new Context {
+      val name = "global"
+      override val typeHintStrategy = StringTypeHintStrategy(when = TypeHintFrequency.WhenNecessary, typeHint = "_t")
+    }
+    context.registerGlobalKeyOverride(remapThis = "id", toThisInstead = "_id")
+    context.registerClassLoader(Play.classloader)
+    context
+  }
+}
+import mongoContext._
+
 case class Registration(username: String, password: String, confirm: String, realName: String)
 
 object Registrations {
